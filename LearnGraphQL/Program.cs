ddcad7cef32b4bdc1392df216b538e8485a4e0ba
Bumindu Yasith <bumiduyasith@gmail.com>
2023-10-27 11:19:56 +0530

    add graph ql server and set query type

--- LearnGraphQL/Program.cs
+++ LearnGraphQL/Program.cs
@@ -1,3 +1,5 @@
+using LearnGraphQL.Schema;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddGraphQLServer().AddQueryType<Query>();
 
 var app = builder.Build();
 
@@ -21,5 +25,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGraphQL();
 
 app.Run();
