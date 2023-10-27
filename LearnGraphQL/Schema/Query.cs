c271dc22ebc94b36e367dd9045eed222eb4b7228
Bumindu Yasith <bumiduyasith@gmail.com>
2023-10-27 11:21:02 +0530

    create query class

---
+++ LearnGraphQL/Schema/Query.cs
@@ -0,0 +1,18 @@
+﻿using LearnGraphQL.Models;
+
+namespace LearnGraphQL.Schema
+{
+    public class Query
+    {
+        public string Test => "testing";
+
+        public IEnumerable<Course> GetAllCourses() 
+        {
+            return new List<Course>()
+            {
+                new Course{Id = 1, Name = "bumindu", GPA = 4.5},
+                new Course{Id = 2, Name = "yasith", GPA = 1.2},
+            };
+        }
+    }
+}
