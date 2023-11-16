f7fcad21c25639d379fa2ed752122303113bbc97
Bumindu Yasith <bumiduyasith@gmail.com>
2023-11-16 15:43:51 +0530

    test endpoint to check client credentials flow

--- USerManangementAPP/Controllers/StaffManageController.cs
+++ USerManangementAPP/Controllers/StaffManageController.cs
@@ -1,13 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OpenIddict.Client;
+using OpenIddict.Validation.AspNetCore;
+using static OpenIddict.Abstractions.OpenIddictConstants;
 
 namespace USerManangementAPP.Controllers
 {
     public class StaffManageController : Controller
     {
-        
         public IActionResult Index()
         {
             return View();
+        }
+
+        [Authorize(AuthenticationSchemes = OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme)]
+        [HttpGet("message")]
+        public async Task<IActionResult> GetMessage()
+        {
+            var subject = User.FindFirst(Claims.Subject)?.Value;
+
+            return Ok(subject);
         }
     }
 }
