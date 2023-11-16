ee97d9b783d48ad50064bc83ac1542991a8ca08f
Bumindu Yasith <bumiduyasith@gmail.com>
2023-11-16 15:47:19 +0530

    add openiddict server

--- USerManangementAPP/Program.cs
+++ USerManangementAPP/Program.cs
@@ -1,5 +1,12 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Quartz;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using USerManangementAPP;
 using USerManangementAPP.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,12 +14,101 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+{
+    options.UseNpgsql(connectionString);
+    options.UseOpenIddict();
+});
+builder.Services.AddHttpClient();
+/*builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();*/
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+/*builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();*/
+
+// JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    options.Lockout.MaxFailedAccessAttempts = 4;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+    options.Password.RequiredLength = 8;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireDigit = true;
+})
+               .AddEntityFrameworkStores<ApplicationDbContext>()
+               .AddDefaultTokenProviders()
+               .AddDefaultUI();
+
+
+/*builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();*/
+
+/*builder.Services.AddDatabaseDeveloperPageExceptionFilter();*/
+
+/*builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();*/
+
+
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddQuartz(options =>
+{
+    options.UseMicrosoftDependencyInjectionJobFactory();
+    options.UseSimpleTypeLoader();
+    options.UseInMemoryStore();
+});
+
+
+builder.Services.AddQuartzHostedService(options => options.WaitForJobsToComplete = true);
+
+
+builder.Services.AddOpenIddict()
+    .AddCore(options =>
+    {
+        options.UseEntityFrameworkCore()
+                    .UseDbContext<ApplicationDbContext>();
+
+        // Enable Quartz.NET integration.
+        options.UseQuartz();
+    })
+    .AddServer(options =>
+    {
+        // Enable the token endpoint.
+        options.SetTokenEndpointUris("connect/token");
+        options.SetIntrospectionEndpointUris("/connect/introspect");
+
+        // Enable the client credentials flow.
+        options.AllowClientCredentialsFlow();
+
+        options.RegisterClaims("customclaim");
+        options.RegisterScopes("api");
+
+        // Register the signing and encryption credentials.
+        options.AddDevelopmentEncryptionCertificate()
+               .AddDevelopmentSigningCertificate();
+
+        options.DisableAccessTokenEncryption();
+
+        // Register the ASP.NET Core host and configure the ASP.NET Core-specific options.
+        options.UseAspNetCore()
+               .EnableTokenEndpointPassthrough();
+    })
+    // Register the OpenIddict validation components.
+    .AddValidation(options =>
+    {
+        // Import the configuration from the local OpenIddict server instance.
+        options.UseLocalServer();
+
+        // Register the ASP.NET Core host.
+        options.UseAspNetCore();
+    });
+
+// Register the worker responsible for seeding the database.
+// Note: in a real world application, this step should be part of a setup script.
+builder.Services.AddHostedService<Worker>();
+
+
 
 var app = builder.Build();
 
@@ -36,9 +132,14 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
+//app.UseSession();
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
 app.MapRazorPages();
 
 app.Run();
+
+
