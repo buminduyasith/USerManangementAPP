ea3223aa38ff7a26d56c5e21cc9bb60977c9724d
Bumindu Yasith <bumiduyasith@gmail.com>
2023-11-16 15:43:17 +0530

    check xerosignin authnetication schema

--- USerManangementAPP/Controllers/HomeController.cs
+++ USerManangementAPP/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
 using USerManangementAPP.Models;
 
 namespace USerManangementAPP.Controllers
@@ -14,6 +16,7 @@ namespace USerManangementAPP.Controllers
             _logger = logger;
         }
 
+
         public IActionResult Index()
         {
             List<StaffMemberDto> inviteList = new List<StaffMemberDto>
@@ -26,9 +29,24 @@ namespace USerManangementAPP.Controllers
             return View(inviteList);
         }
 
-        [Authorize]
-        public IActionResult Privacy()
+        [HttpGet]
+        [Authorize(AuthenticationSchemes = "XeroSignIn")]
+        public async Task<IActionResult> Privacy()
         {
+
+            var x = User;
+
+            var accessTokenww = await HttpContext.GetTokenAsync("XeroSignIn", "access_token");
+            var accessTokenww2 = await HttpContext.GetTokenAsync("XeroSignIn", "refresh_token");
+            var accessTokenwsd = await HttpContext.GetTokenAsync("xerosignintemp", "access_token");
+            var accessTokenwsd2 = await HttpContext.GetTokenAsync("xerosignintemp", "refresh_token");
+
+            // var handler = new JwtSecurityTokenHandler();
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            var IdToken = await HttpContext.GetTokenAsync("id_token");
+            var accessToke2n = await HttpContext.GetTokenAsync("XeroSignIn");
+            // var token = handler.ReadJwtToken(IdToken);
+            await HttpContext.SignOutAsync("xerosignintemp");
             return View();
         }
 
@@ -36,6 +54,14 @@ namespace USerManangementAPP.Controllers
         public IActionResult Edit(string email)
         {
             ViewBag.Email = email;
+            return View();
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult Test()
+        {
+            var x = User;
             return View();
         }
 
